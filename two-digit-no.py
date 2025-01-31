5a4514fd2c5912c182035b48ca4a26a018e9acec
Dave <davenjenga098@gmail.com>
2025-01-31 23:39:39 +0300

    added more codility

--- two-digit-no.py
+++ two-digit-no.py
@@ -9,6 +9,7 @@
 # 1. Given S = "43798", the function should return 141. The chosen fragments are "43" and "98": "43 7 98"
 # 2. Given S = "00101", the function should return 10. The chosen fragments are "00" and "10": "00 10 1". 
 # Note that fragments "01" and "10" cannot be chosen as they overlap.
+
 # 3. Given S = "0332331", the function should return 66. The chosen fragments are "33" and "33": "0 33 2 33 1".
 # 4. Given S = "00331", the function should return 34. The chosen fragments are "03" and "31": "0 03 31"
 # Assume that:
