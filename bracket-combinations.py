54ff2fad646b0c82e4face327e02437947e55d63
Dave-elite <davenjenga098@gmail.com>
2025-02-14 10:37:22 +0300

    added bracket combinations and solved partally

---
+++ bracket-combinations.py
@@ -0,0 +1,29 @@
+# Bracket Combinations
+# Have the function BracketCombinations(num) read num which will be an integer greater than or equal to zero, and return 
+# the number of valid combinations that can be formed with num pairs of parentheses. For example, if the input is 3,
+#  then the possible combinations of 3 pairs of parenthesis, namely: ()()(), are ()()(), ()(()), (())(), ((())), and (()()). 
+#  There are 5 total combinations when the input is 3, so your program should return 5.
+# Examples
+# Input: 3
+# Output: 5
+# Input: 2
+# Output: 2
+
+def BracketCombinations(num):
+  
+  #if the num argument is 0 then this means there are no pairs of parenthesis to arrange.
+  #The only solid comninations is an empty string which count as 1 valid arrangemnent so we return 1
+  if num == 0:
+    return 1
+  
+
+  count = 0
+  for i in range(num):
+    # print(BracketCombinations(i))
+    # print(BracketCombinations(num - 1 - i))
+    count += BracketCombinations(i) * BracketCombinations(num - 1 - i)
+  return count
+
+
+# keep this function call here 
+print(BracketCombinations(input()))
