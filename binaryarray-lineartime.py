0448fbb28e1281bdd6fb6ae0ecb46f77ca652152
Dave-elite <davenjenga098@gmail.com>
2025-03-14 10:03:10 +0300

    binarry linear time solved part 2

--- binaryarray-lineartime.py
+++ binaryarray-lineartime.py
@@ -13,43 +13,74 @@
 #     arrange = sorted(A)
 #     return arrange
 
-
-
-
-
-
-
 def solution(A):
-    # count number of 0's
     zeros = A.count(0)
     print(zeros)
-    #put the zeros at the beginging
-    '''
-    variable k is initialized to 0. This will be used as the index to place the zeros at the 
-    begining of the list 
-    '''
     k = 0
-    '''
-    the while loop runs as longs there are zeros < 0 in each iterations of the loop 
-    A[k] = 0 it sets k-th element of A to 0.
-    zeros = zeros - 1 : this decreases the count of the remaining zeros
-    k = k + 1 it increments k moving to the next position in the list for placing the next 0.
-
-    '''
     while zeros:
         A[k] = 0
-        
         zeros = zeros - 1
         k = k + 1
-        # print(k)
-    '''
-    After the while loop the remaining elements in the list (starting from the index k) are set to 1 this done using for loop
-    
-    '''
     for k in range(k, len(A)):
         A[k] = 1
-    
-    sorted(A)
     print(A)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(A):
+#     # count number of 0's
+#     zeros = A.count(0)
+#     print(zeros)
+#     #put the zeros at the beginging
+#     '''
+#     variable k is initialized to 0. This will be used as the index to place the zeros at the 
+#     begining of the list 
+#     '''
+#     k = 0
+#     '''
+#     the while loop runs as longs there are zeros < 0 in each iterations of the loop 
+#     A[k] = 0 it sets k-th element of A to 0.
+#     zeros = zeros - 1 : this decreases the count of the remaining zeros
+#     k = k + 1 it increments k moving to the next position in the list for placing the next 0.
+
+#     '''
+#     while zeros:
+#         A[k] = 0
+        
+#         zeros = zeros - 1
+#         k = k + 1
+#         # print(k)
+#     '''
+#     After the while loop the remaining elements in the list (starting from the index k) are set to 1 this done using for loop
+
+#     '''
+#     for k in range(k, len(A)):
+#         A[k] = 1
+    
+#     sorted(A)
+#     print(A)
 
 print(solution([ 1, 0, 1, 0, 1, 0, 0, 1 ]))
